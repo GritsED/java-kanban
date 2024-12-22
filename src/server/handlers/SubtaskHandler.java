07ee11c62f77f49a3b82de5d8f1588c11552f073
Эдик <Hy6Jlo1998@yandex.ru>
2024-12-22 14:46:55 +0300

    Add new classes EpicHandler and SubtaskHandler

---
+++ src/server/handlers/SubtaskHandler.java
@@ -0,0 +1,131 @@
+package server.handlers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sun.net.httpserver.HttpExchange;
+import model.Subtask;
+import model.Task;
+import server.adapter.DurationAdapter;
+import server.adapter.LocalDateTimeAdapter;
+import service.TaskManager;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class SubtaskHandler extends TaskHandler {
+    private final Charset utf8 = StandardCharsets.UTF_8;
+    private final Gson gson = new GsonBuilder().serializeNulls().registerTypeAdapter(Duration.class, new DurationAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter()).create();
+
+
+    public SubtaskHandler(TaskManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+        String requestPath = exchange.getRequestURI().getPath();
+
+
+        switch (requestMethod) {
+            case "GET":
+                handleGet(exchange, requestPath);
+                break;
+            case "POST":
+                handlePost(exchange, requestPath);
+                break;
+            case "DELETE":
+                handleDelete(exchange, requestPath);
+                break;
+            default:
+                sendNotFound(exchange);
+        }
+    }
+
+    private void handleGet(HttpExchange exchange, String requestPath) throws IOException {
+        String[] pathParts = requestPath.split("/");
+        Optional<Integer> idPath = getId(requestPath);
+
+        try {
+            if (pathParts.length == 2 && pathParts[1].equals("subtasks")) {
+                String response = gson.toJson(manager.getAllSubtasks());
+                sendText(exchange, response, 200);
+            } else if (pathParts.length == 3 && pathParts[1].equals("subtasks") && idPath.isPresent()
+                    && Pattern.matches("\\d+", pathParts[2])) {
+                int id = idPath.get();
+                if (manager.getSubtaskById(id) != null) {
+                    String response = gson.toJson(manager.getSubtaskById(id));
+                    sendText(exchange, response, 200);
+                } else {
+                    sendNotFound(exchange);
+                }
+            } else {
+                sendNotFound(exchange);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            sendInternalError(exchange);
+        }
+    }
+
+    private void handlePost(HttpExchange exchange, String requestPath) throws IOException {
+        String body = new String(exchange.getRequestBody().readAllBytes(), utf8);
+        String[] pathParts = requestPath.split("/");
+        Optional<Integer> idPath = getId(requestPath);
+
+        try {
+            if (body.isEmpty()) {
+                sendText(exchange, "The body is empty", 400);
+                return;
+            }
+            if (pathParts.length == 2 && pathParts[1].equals("subtasks")) {
+                Task addedSubtask = manager.addNewSubtask(gson.fromJson(body, Subtask.class));
+                if (Objects.nonNull(addedSubtask)) {
+                    sendText(exchange, gson.toJson(addedSubtask), 201);
+                } else {
+                    sendHasInteractions(exchange);
+                }
+            } else if (pathParts.length == 3 && pathParts[1].equals("subtasks") && idPath.isPresent()
+                    && Pattern.matches("\\d+", pathParts[2])) {
+                Task updatedSubtask = manager.updateSubtask(gson.fromJson(body, Subtask.class));
+                if (Objects.nonNull(updatedSubtask)) {
+                    sendText(exchange, gson.toJson(updatedSubtask), 201);
+                } else {
+                    sendHasInteractions(exchange);
+                }
+            } else {
+                sendNotFound(exchange);
+            }
+        } catch (Exception e) {
+            sendInternalError(exchange);
+        }
+    }
+
+    private void handleDelete(HttpExchange exchange, String requestPath) throws IOException {
+        Optional<Integer> idPath = getId(requestPath);
+
+        if (idPath.isPresent()) {
+            Task deletedSubtask = manager.getSubtaskById(idPath.get());
+            manager.deleteSubtaskById(idPath.get());
+
+            try {
+                if (Objects.isNull(deletedSubtask)) {
+                    sendNotFound(exchange);
+                } else {
+                    sendText(exchange, "Task was deleted", 200);
+                }
+            } catch (Exception e) {
+                sendInternalError(exchange);
+            }
+        } else {
+            sendNotFound(exchange);
+        }
+    }
+}
